0d920259870415499c6a0f908273ab5b5441b6a2
chris-base <baseappsdev@gmail.com>
2022-03-22 20:58:39 -0500

    added filter dropdown

--- src/Components/FilterComponent.js
+++ src/Components/FilterComponent.js
@@ -1,13 +1,47 @@
+import { useState, useRef, useEffect } from "react";
 import "../Styles/FilterStyles.css";
 import iconDropLight from "./images/icon-drop-light.svg";
 import iconDropDark from "./images/icon-drop-dark.svg";
 
 const FilterComponent = () => {
-  return (
-    <div id='filterContainer'>
-      <p id='filterText'>Filter by Region</p>
+  const [filterOpen, setFilterOpen] = useState(false);
 
-      <div id='filterDropButton' style={{ backgroundImage: "url( " + iconDropLight + ")" }} />
+  const wrapperRef = useRef(null);
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside, false);
+    return () => {
+      document.removeEventListener("click", handleClickOutside, false);
+    };
+  }, []);
+
+  const handleClickOutside = (event) => {
+    if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+      setFilterOpen(false);
+    }
+  };
+
+  console.log("HERE");
+
+  return (
+    <div id='filterContainer' ref={wrapperRef}>
+      <div id='filterUnexpandedContainer' onClick={() => setFilterOpen(!filterOpen)}>
+        <p id='filterText'>Filter by Region</p>
+
+        <div id='filterDropButton' style={{ backgroundImage: "url( " + iconDropLight + ")" }} />
+      </div>
+
+      {filterOpen ? (
+        <div id='filterExpandedContainer'>
+          <p className='filterText'>Africa</p>
+          <p className='filterText'>America</p>
+          <p className='filterText'>Asia</p>
+          <p className='filterText'>Europe</p>
+          <p className='filterText'>Oceania</p>
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
