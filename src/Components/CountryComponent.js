87bf3a707cb94526948b1c247a61cbcf210eeb8a
chris-base <baseappsdev@gmail.com>
2022-04-01 19:57:13 -0500

    started dark mode for countries list/home page

--- src/Components/CountryComponent.js
+++ src/Components/CountryComponent.js
@@ -1,26 +1,44 @@
 import "../Styles/CountryStyles.css";
 
-const CountryComponent = ({ countryInfo, setCurrCountry, index }) => {
+const CountryComponent = ({ countryInfo, setCurrCountry, index, darkLightMode }) => {
   return (
-    <div id='countryComponentContainer' onClick={() => setCurrCountry(index)}>
+    <div
+      id='countryComponentContainer'
+      onClick={() => setCurrCountry(index)}
+      style={darkLightMode ? { backgroundColor: "#2b3945" } : { backgroundColor: "white" }}
+    >
       <div id='countryFlagImage' style={{ backgroundImage: "url(" + countryInfo.flags.svg + ")" }} />
 
       <div id='countryInfoContainer'>
-        <p id='countryInfoName'>{countryInfo.name.common}</p>
+        <p id='countryInfoName' style={darkLightMode ? { color: "#ffffff" } : { color: "#111517" }}>
+          {countryInfo.name.common}
+        </p>
 
         <div className='countryInfoSectionContainer'>
-          <p className='countryInfoBoldText'>Population:</p>
-          <p className='countryInfoText'>{countryInfo.population.toLocaleString()}</p>
+          <p className='countryInfoBoldText' style={darkLightMode ? { color: "#ffffff" } : { color: "#111517" }}>
+            Population:
+          </p>
+          <p className='countryInfoText' style={darkLightMode ? { color: "#ffffff" } : { color: "#111517" }}>
+            {countryInfo.population.toLocaleString()}
+          </p>
         </div>
 
         <div className='countryInfoSectionContainer'>
-          <p className='countryInfoBoldText'>Region:</p>
-          <p className='countryInfoText'>{countryInfo.region}</p>
+          <p className='countryInfoBoldText' style={darkLightMode ? { color: "#ffffff" } : { color: "#111517" }}>
+            Region:
+          </p>
+          <p className='countryInfoText' style={darkLightMode ? { color: "#ffffff" } : { color: "#111517" }}>
+            {countryInfo.region}
+          </p>
         </div>
 
         <div className='countryInfoSectionContainer'>
-          <p className='countryInfoBoldText'>Capital:</p>
-          <p className='countryInfoText'>{countryInfo.capital}</p>
+          <p className='countryInfoBoldText' style={darkLightMode ? { color: "#ffffff" } : { color: "#111517" }}>
+            Capital:
+          </p>
+          <p className='countryInfoText' style={darkLightMode ? { color: "#ffffff" } : { color: "#111517" }}></p>
+            {countryInfo.capital}
+          </p>
         </div>
       </div>
     </div>
